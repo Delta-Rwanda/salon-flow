a12d020eaac6792ceb806593a5b84ea99f2b2fc5
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2025-03-15 10:19:38 +0200

    Added eslint to the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <div>
       <h1>Salon flow</h1>
+      <span>Now I am making some changes</span>
       <p>
         Salon management app that simplifies appointment scheduling, customer management, and daily
         operations for salon owners and staff.
