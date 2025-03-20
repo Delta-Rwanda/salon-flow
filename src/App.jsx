892c952d1ae4abd9c815d32b84b3b3e21c12744d
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2025-03-20 09:53:17 +0200

    Revert "Setup Prettier"

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ const App = () => {
     <div>
       <h1>Salon flow</h1>
       <p>
-        Salon management app that simplifies appointment scheduling, customer
-        management, and daily operations for salon owners and staff.
+        Salon management app that simplifies appointment scheduling, customer management, and daily
+        operations for salon owners and staff.
       </p>
     </div>
   );
-};
-export default App;
+}
+export default App
