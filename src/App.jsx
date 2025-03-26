838c147a130be20a596307951c090ffdbc792d91
benidama <benidama02@gmail.com>
2025-03-26 19:03:18 +0000

    Setup of tailwind css using vite

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
         Salon management app that simplifies appointment scheduling, customer
         management, and daily operations for salon owners and staff.
       </p>
+      <button className="bg-blue-500 text-white p-4 rounded">Click Me</button>
     </div>
   );
 };
