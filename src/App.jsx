b78103ed425445fb6413376999f9354beb7c43cb
Mucyo Papy Blaise <mucyoblaise86@gmail.com>
2025-04-02 00:53:19 +0200

    remove console and auto-fix on EsLint

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Navigation from './components/Navigation';
 const App = () => {
-  console.log("Wassup! husky and linte-staged ")
   return (
     <div>
       <Navigation />
