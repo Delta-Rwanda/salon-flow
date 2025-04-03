8d3122a151da23d2b626665c5e7ce0129fb626a2
Mucyo Papy Blaise <mucyoblaise86@gmail.com>
2025-04-03 18:14:48 +0200

    fixing formatting issues

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <div>
       <Router>
         <Navigation />
-        <h1 className='tex-blue-500'>Salon flow</h1>
+        <h1 className="tex-blue-500">Salon flow</h1>
         <p>
           Salon management app that simplifies appointment scheduling, customer
           management, and daily operations for salon owners and staff.
