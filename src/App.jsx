ebe6de38df74aa3d008ee2d94dab2ea9ca5c61d2
Elie MANIZABAYO <manizabayoelie.com>
2025-04-05 21:14:05 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const App = () => {
   return (
     <div>
@@ -6,6 +7,47 @@ const App = () => {
         Salon management app that simplifies appointment scheduling, customer
         management, and daily operations for salon owners and staff.
       </p>
+=======
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import Navigation from './components/Navigation';
+import LogIn from './pages/LogIn';
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Navigation />
+        <h1>Salon flow</h1>
+        <p>
+          Salon management app that simplifies appointment scheduling, customer
+          management, and daily operations for salon owners and staff.
+        </p>
+        <button className="bg-blue-500 text-white p-4 rounded">Click ME</button>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a className="text-7xl text-blue-700" href="/about">
+                About
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="*" element={<LogIn />} />
+          </Routes>
+        </div>
+      </Router>
+>>>>>>> Stashed changes
     </div>
   );
 };
---
+++ src/pages/LogIn.jsx
@@ -0,0 +1,12 @@
+
+
+const LogIn = () => {
+    return (
+      <div>
+        <h1>Login</h1>
+      </div>
+    );
+  };
+  
+  export default LogIn;
+  
