1d97bb239641c788cc5076e11b30bd219101cfdc
benidama <benidama02@gmail.com>
2025-04-10 19:15:45 +0000

    switch to another branch

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,56 @@
 import { Link } from 'react-router-dom';
+import { ChevronDown, ChevronUp } from 'lucide-react';
+import { useState } from 'react';
+import { MdLanguage } from 'react-icons/md';
+import { MdLightMode } from 'react-icons/md';
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [name, setName] = useState('English');
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
+    <nav className="flex flex-row justify-around items-center w-full h-16 bg-[#F8F9FA]">
+      <h1>SALON FLOW</h1>
+      <ul className="flex flex-row gap-3">
+        <div className="flex flex-row justify-center items-center">
+          <MdLanguage className="text-black text-3xl" />
+          <p className="text-[#555555] font-medium text-[20px]">
+            <span className="text-[#555555]">{name}</span>
+          </p>
+          <div
+            className=" w-10 h-6 flex flex-col justify-center items-center"
+            onClick={handleOpen}
+          >
+            {isOpen ? (
+              <ChevronDown className="w-10 h-6 text-[#555555]" />
+            ) : (
+              <ChevronUp className="w-10 h-6 text-[#555555]" />
+            )}
+          </div>
+
+          {isOpen && (
+            <div className="bg-white w-36 p-3 ">
+              {['English', 'French', 'Kinyarwanda'].map((n) => (
+                <p
+                  key={n}
+                  className="hover:bg-yellow-400 cursor-pointer"
+                  onClick={() => setName(n)}
+                >
+                  {n}
+                </p>
+              ))}
+            </div>
+          )}
+        </div>
+        <MdLightMode className="text-3xl text-center" />
+        <li className="bg-[#FFB6C1] pr-3 pl-3 pt-0.5 pb-0.5 rounded-md">
+          <Link to="/">Sign In</Link>
         </li>
-        <li>
-          <Link to="/about">About</Link>
+        <li className="border-sold border-[#FFB6C1] border-4 pr-3 pl-3 pt-0.5 pb-0.5 rounded-md">
+          <Link to="/">Create Account</Link>
         </li>
       </ul>
     </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import Footer from '../components/Footer';
 const Home = () => {
   return (
     <div>
       <h1>Home </h1>
+      <Footer />
     </div>
   );
 };
