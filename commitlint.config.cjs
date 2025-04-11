ddcf56f6361afc26840e6fb3ea0b260a020b60d3
Elie MANIZABAYO <manizabayoelie.com>
2025-04-11 21:01:35 +0200

    test: add sentence case validation

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -14,6 +14,6 @@ module.exports = {extends: ['@commitlint/config-conventional'],
         'revert'
       ]
     ],
-    'subject-case': [2, 'always', 'sentence-case']
+    'subject-case': [0]
   }
 };
