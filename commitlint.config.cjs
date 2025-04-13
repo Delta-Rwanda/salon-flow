db6e40eb7944f1bbb6e3131e3b894e0b7c79693f
Elie MANIZABAYO <manizabayoelie.com>
2025-04-13 14:27:52 +0200

    set up modified

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,19 +1,1 @@
 module.exports = {extends: ['@commitlint/config-conventional'],}
-//     rules: {
-//     'type-enum': [
-//       2,
-//       'always',
-//       [
-//         'feat',
-//         'fix',
-//         'docs',
-//         'style',
-//         'refactor',
-//         'test',
-//         'chore',
-//         'revert'
-//       ]
-//     ],
-//     'subject-case': [0]
-//   }
-// };
