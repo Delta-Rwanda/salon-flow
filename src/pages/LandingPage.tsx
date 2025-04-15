e0316b9e833336964fa97c9af253d3becff25814
Mucyo Papy Blaise <mucyoblaise86@gmail.com>
2025-04-15 21:33:01 +0200

    feature: landing-page/home

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Home from "../components/Home"
+
+const LandingPage:React.FC = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default LandingPage
