6ab751710b80f7e85998889635b59f45c39cfc7c
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2025-04-16 10:41:55 +0200

    Setup CommitLint and integrate with Husky (#33)
    * first commit
    
    * invalid message
    
    * resolve conflicts
    
    * reslove conflicts
    
    * fix conflicts
    
    * committed
    
    * errors
    
    * fix: errors
    
    * errors
    
    * foo
    
    * foo
    
    * foo
    
    * fix: errors
    
    * solve:  errors
    
    * solve:  errors
    
    * fix: login-page
    
    * feat: add  test rule login-page
    
    * test: Add sentence case validation
    
    * test: add sentence case validation
    
    * feat: setup modified
    
    * set up modified
    
    * feat: setup modified
    
    * feat: setup modified
    
    * test: npm run format:fix
    
    * chore: setup commit lint and integrate with husky
    
    ---------
    
    Co-authored-by: Elie MANIZABAYO <manizabayoelie.com>
    Co-authored-by: Ivad <yvesyvad@gmail.com>

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,10 @@
 const About = () => {
-  return <div className="bg-black">About Page</div>;
+  return (
+    <div className="bg-black">
+      About Page
+      <h1>Welcome to our online salon</h1>
+    </div>
+  );
 };
 
 export default About;
