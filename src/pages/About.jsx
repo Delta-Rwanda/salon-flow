4256ff8cf7c6e4ff2c5a74cd340aa1b0ceb993c0
Elie MANIZABAYO <manizabayoelie.com>
2025-04-16 09:31:54 +0200

    test: npm run format:fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,10 @@
 const About = () => {
-  return( <div className="bg-black">About Page
-            <h1>Welcome to our  online salon</h1>
-          </div>
-        )
+  return (
+    <div className="bg-black">
+      About Page
+      <h1>Welcome to our online salon</h1>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,16 +1,10 @@
-
-
 const LogIn = () => {
-    return (
-        <form action="">
-            <h1>SALON FLOW</h1>
-            <div>
-               
-            </div>
-        </form>
-      
-    );
-  };
-  
-  export default LogIn;
-  
+  return (
+    <form action="">
+      <h1>SALON FLOW</h1>
+      <div></div>
+    </form>
+  );
+};
+
+export default LogIn;
