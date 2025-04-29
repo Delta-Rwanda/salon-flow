9c4022e74cdb62122a54b4ba3d96c4cd54b2465a
Elie MANIZABAYO <manizabayoelie.com>
2025-04-29 22:08:25 +0200

    fix: format codes

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -50,14 +50,14 @@ const LogIn = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white p-4 font-poppins">
-      <div className="w-full max-w-md">
+      <div className="w-full max-w-md px-4 sm:px-6 lg:px-8">
         {/*Title */}
-        <h1 className="text-center text-2xl font-medium text-gray-800 mb-6">
+        <h1 className="text-center text-2xl sm:text-3xl font-medium text-gray-800 mb-6">
           SALON FLOW
         </h1>
 
         {/* Sign in text */}
-        <p className="text-2xl text-center mb-8 font-poppins font-regular">
+        <p className="text-xl sm:text-2xl text-center mb-6 sm:mb-8 font-poppins font-regular">
           Sign in to unlock the best of
           <br />
           Salon-Flow.
@@ -68,7 +68,7 @@ const LogIn = () => {
           <div className="mb-4">
             <label
               htmlFor="email"
-              className="block text-[16px] font-poppins font-regular text-gray-700 mb-1"
+              className="block text-[14px] sm:text-[16px] font-poppins font-regular text-gray-700 mb-1"
             >
               Email
             </label>
@@ -77,11 +77,13 @@ const LogIn = () => {
               type="email"
               value={email}
               onChange={(event) => setEmail(event.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#505050] bg-[#F4F4F4]"
+              className="w-full px-3 py-2 text-sm sm:text-base border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#505050] bg-[#F4F4F4]"
               required
             />
             {errors.email && (
-              <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+              <p className="text-red-500 text-xs sm:text-sm mt-1">
+                {errors.email}
+              </p>
             )}
           </div>
 
@@ -89,7 +91,7 @@ const LogIn = () => {
           <div className="mb-2">
             <label
               htmlFor="password"
-              className="block text-sm text-gray-700 mb-1"
+              className="block text-[14px] sm:text-[16px] text-gray-700 mb-1"
             >
               Password
             </label>
@@ -98,19 +100,21 @@ const LogIn = () => {
               type="password"
               value={password}
               onChange={(event) => setPassword(event.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#505050] bg-[#F4F4F4]"
+              className="w-full px-3 py-2 text-sm sm:text-base border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#505050] bg-[#F4F4F4]"
               required
             />
             {errors.password && (
-              <p className="text-red-500 text-sm mt-1">{errors.password}</p>
+              <p className="text-red-500 text-xs sm:text-sm mt-1">
+                {errors.password}
+              </p>
             )}
           </div>
 
           {/* Forgot Password Link */}
-          <div className="flex justify-start mb-6">
+          <div className="flex justify-start mb-4 sm:mb-6">
             <a
               href="#"
-              className="text-xl font-poppins font-semibold text-gray-600 hover:text-gray-800"
+              className="text-sm sm:text-xl font-poppins font-semibold text-gray-600 hover:text-gray-800"
             >
               Forgot your password?
             </a>
@@ -119,16 +123,16 @@ const LogIn = () => {
           {/* Sign In Button */}
           <button
             type="submit"
-            className="w-full bg-[#ffb6c1e6] text-xl font-poppins font-semibold text-black py-3 rounded-md hover:bg-[#ffb6c080] transition duration-200"
+            className="w-full bg-[#DB2777] text-sm sm:text-xl font-poppins font-semibold text-black py-2 sm:py-3 rounded-md hover:bg-[#db277893] transition duration-200"
           >
             Sign In
           </button>
         </form>
 
         {/* OR Divider */}
-        <div className="flex items-center my-6">
+        <div className="flex items-center my-4 sm:my-6">
           <div className="flex-grow border-t border-gray-300"></div>
-          <span className="px-4 text-[16px] text-gray-500 font-poppins font-semibold">
+          <span className="px-4 text-sm sm:text-[16px] text-gray-500 font-poppins font-semibold">
             OR
           </span>
           <div className="flex-grow border-t border-gray-300"></div>
@@ -137,17 +141,19 @@ const LogIn = () => {
         {/* Sign In Button With Google */}
         <button
           type="button"
-          className="w-full flex items-center justify-center bg-gray-900 text-white py-3 rounded-md hover:bg-gray-800 transition duration-200"
+          className="w-full flex items-center justify-center bg-gray-900 text-white py-2 sm:py-3 rounded-md hover:bg-gray-800 transition duration-200"
         >
-          <span className="pr-6">
-            <FcGoogle size={30} />
+          <span className="pr-2 sm:pr-6">
+            <FcGoogle size={24} className="sm:size-[30px]" />
           </span>
-          <p className="text-xl font-semibold">Continue with Google</p>
+          <p className="text-sm sm:text-xl font-semibold">
+            Continue with Google
+          </p>
         </button>
 
         {/* Sign Up Link */}
-        <div className="text-center mt-6">
-          <p className="text-gray-600 text-xl font-poppins font-mixed">
+        <div className="text-center mt-4 sm:mt-6">
+          <p className="text-gray-600 text-sm sm:text-xl font-poppins font-bold h-[24px] sm:h-[30px] w-full sm:w-[324px] mx-auto">
             Don&apos;t have an account?
             <a href="#" className="text-[#FFB6C1] ml-1 hover:text-[#ffb6c5d7]">
               Sign Up
